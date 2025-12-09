53f4533ab04ea57eec5fb7cd1a6d0bb46d4f2848
studgenius <louisonliberte@gmail.com>
2025-12-09 21:13:52 +0100

    Update layout.tsx, layout.tsx(dashboard), all page.tsx(dashboard), CountChart.tsx, menu.tsx, Navbar.tsx, and UserCard.tsx

---
+++ src/app/(dashboard)/parent/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ParentPage = () => {
+    return (
+        <div className=''>
+            ParentPage
+        </div>
+    )
+}
+
+export default ParentPage
---
+++ src/app/(dashboard)/teacher/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const TeacherPage = () => {
+    return (
+        <div className=''>
+            TeacherPage
+        </div>
+    )
+}
+
+export default TeacherPage
